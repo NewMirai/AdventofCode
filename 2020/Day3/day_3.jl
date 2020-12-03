82b43c5f993617f0c980b860647e4a6acf699ce6
alangel <alainanghelidi@gmail.com>
2020-12-03 20:18:27 +0100

    Added optimizations after comparing benchmarks

--- 2020/Day3/day_3.jl
+++ 2020/Day3/day_3.jl
@@ -2,7 +2,6 @@ using BenchmarkTools
 
 path = joinpath(@__DIR__, "2020", "Day3", "datas", "input.txt")
 input = readlines(path)
-const TREE_SYMBOL = '\u0023'
 
 function toboggan_solution(slope::Tuple{Int,Int}, input::Array{String,1})
     w = length(input[1])
@@ -10,11 +9,11 @@ function toboggan_solution(slope::Tuple{Int,Int}, input::Array{String,1})
     tree_count = 0
     right = 1
     down = 1
-    for i in 2:h
+    @fastmath @inbounds @simd for i in 2:h
         if down < h
             right = mod1(right + slope[1], w)
             down += slope[2]
-            if input[down][right] == TREE_SYMBOL
+            @fastmath @inbounds if input[down][right] == '\u0023'
                 tree_count += 1
             end
         end
@@ -24,17 +23,17 @@ end
 
 function toboggan_solution(slopes::Array{Tuple{Int,Int},1}, input::Array{String,1})
     tree_count = 1
-    for slope in slopes
-        tree_count *= toboggan_solution(slope, input)
+    @fastmath @inbounds @simd for i ∈ 1:length(slopes)
+        @fastmath @inbounds tree_count *= toboggan_solution(slopes[i], input)
     end
     tree_count
 end
 
-@btime toboggan_solution((3, 1), input)
+@btime toboggan_solution((3, 1), $input)
 @btime toboggan_solution([
     (1, 1),
     (3, 1),
     (5, 1),
     (7, 1),
     (1, 2)
-],input)
+],$input)
